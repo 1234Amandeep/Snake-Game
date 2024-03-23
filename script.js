3641f655ec4aada898efe25e4ac79cc5e96833c5
Amandeep Singh <samandeep8057@gmail.com>
2024-03-23 13:16:37 +0530

    Initial setup is done!

---
+++ script.js
@@ -0,0 +1,11 @@
+// ☘️☘️☘️ initial setup
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+// 🔥🔥🔥 write code from here...
