45159ba09a706cd11dc231b4a8a01f29f02bb664
Amandeep Singh <samandeep8057@gmail.com>
2024-03-28 12:55:44 +0530

    3 features build, snake food drawn randomly on the board, snake can eat it and every subsiquent meal it gets longer, and finally keeping track of how many meals he had up until now

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ const lengthOfBoard = 100;
 const lengthOfSnake = 4;
 const snake = [];
 let direction = "right";
+let score = 0;
 const foodPos = {
-  x: 6,
-  y: 5,
+  x: parseInt(Math.random() * 9 + 0),
+  y: parseInt(Math.random() * 9 + 0),
 };
 
 const initSnake = () => {
   for (let i = lengthOfSnake - 1; i >= 0; i--) {
-    snake.push({ x: i, y: 0 });
+    snake.push({ x: i + 2, y: 3 });
   }
 };
 initSnake();
@@ -50,8 +51,18 @@ const moveSnake = () => {
     HeadY++;
   }
   let tail = snake.pop();
+  let tailX = tail.x;
+  let tailY = tail.y;
+
   tail.x = HeadX;
   tail.y = HeadY;
+  if (tail.x == foodPos.x && tail.y == foodPos.y) {
+    snake.push({ tailX, tailY });
+    foodPos.x = parseInt(Math.random() * 9 + 0);
+    foodPos.y = parseInt(Math.random() * 9 + 0);
+    score++;
+    console.log(score);
+  }
   snake.unshift(tail);
 };
 
@@ -67,23 +78,26 @@ const drawBoard = (length) => {
 };
 drawBoard(lengthOfBoard);
 
-// draw Snake
-const drawSnake = () => {
-  for (let i = 0; i < board.length; i++) {
-    drawCell(board[i].x, board[i].y, "black", "white");
-  }
-  console.log(snake);
-  for (let i = 0; i < snake.length; i++) {
-    drawCell(snake[i].x, snake[i].y, "lime", "black");
-  }
-  moveSnake();
-};
-drawSnake();
 // draw food
 const drawFood = () => {
-  const x = foodPos.x * size + size / 2;
-  const y = foodPos.y * size + size / 2;
+  const snakeHeadX = snake[0].x - 1;
+  const snakeHeadY = snake[0].y;
+  const snakeTailX = snake[snake.length - 1].x - 1;
+  const snakeTailY = snake[snake.length - 1].y;
+  let x = foodPos.x;
+  let y = foodPos.y;
 
+  if (
+    x <= snakeHeadX &&
+    x >= snakeTailX &&
+    y <= snakeHeadY &&
+    y >= snakeTailY
+  ) {
+    foodPos.x = parseInt(Math.random() * 9 + 0);
+    foodPos.y = parseInt(Math.random() * 9 + 0);
+  }
+  x = x * size + size / 2;
+  y = y * size + size / 2;
   ctx.beginPath();
   ctx.fillStyle = "purple";
   ctx.arc(x, y, size / 2 - 5, 0, 2 * Math.PI);
@@ -91,14 +105,35 @@ const drawFood = () => {
 };
 drawFood();
 
-// 📽️📽️📽️ animation
-const animation = () => {
-  // moveSnake("right");
-
-  drawSnake();
+// draw Snake
+const drawSnake = () => {
+  // re-draw board so that snake will get erased from the game in that moment
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < board.length; i++) {
+    drawCell(board[i].x, board[i].y, "black", "white");
+  }
+  // 🔥🔥🔥 drawing score
+  ctx.font = "24px sans";
+  ctx.fillStyle = "black";
+  ctx.fillText(`Score: ${score}`, 330, 150);
+  drawFood();
+  // snake will be drawn according to the snake array's current state
+  for (let i = 0; i < snake.length; i++) {
+    drawCell(snake[i].x, snake[i].y, "lime", "black");
+  }
+  moveSnake();
 };
+drawSnake();
 
+// 📽️📽️📽️ animation
+// const animation = () => {
+//   drawSnake();
+//   window.requestAnimationFrame(animation);
+// };
+
+let interval;
 document.addEventListener("keydown", (e) => {
+  clearInterval(interval);
   const key = e.which;
   if (key == 39 && direction != "left") {
     direction = "right";
@@ -111,6 +146,6 @@ document.addEventListener("keydown", (e) => {
   }
   console.log("hey", direction);
   // Game loop
-  // setInterval(drawSnake, 1000);
-  window.requestAnimationFrame(drawSnake);
+  interval = setInterval(drawSnake, 400);
+  // window.requestAnimationFrame(drawSnake);
 });
